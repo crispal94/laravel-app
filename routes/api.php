24687033364bf80ac56667ddd4144cd2dee83b14
Christian Palacios <crispal94@hotmail.com>
2023-07-23 00:22:10 -0500

    feat: Add API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\ProfessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResources([
+    'person' => PersonController::class,
+    'profession' => ProfessionController::class,
+]);
