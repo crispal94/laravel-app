33b895b0bda671708e2e4d2ba64a7044f67eeafd
Christian Palacios <crispal94@hotmail.com>
2023-07-23 00:17:08 -0500

    feat: Create factories to generate data to the tables

---
+++ database/factories/ProfessionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profession>
+ */
+class ProfessionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->jobTitle(),
+            'description' => fake()->realText(60),
+            'average_salary' => rand(1000, 99999) / 100,
+        ];
+    }
+}
