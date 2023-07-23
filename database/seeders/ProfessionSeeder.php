a5f528dbc06bbdfcbbc9dc0adeeef40959bbb978
Christian Palacios <crispal94@hotmail.com>
2023-07-23 00:18:48 -0500

    feat: Create seeders to populate the tables with fake data

---
+++ database/seeders/ProfessionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profession;
+use Illuminate\Database\Seeder;
+
+class ProfessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Profession::factory()->count(10)->create();
+    }
+}
